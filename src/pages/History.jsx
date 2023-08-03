bf05518ddc5c94ff956dadfa0b10127df1f70206
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-08-03 17:21:23 -0300

    Atualizando projeto

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,19 +1,21 @@
 import styles from './History.module.css'
-
-import { FaReact, FaHtml5, FaCss3Alt } from 'react-icons/fa'
-import { SiTypescript, SiJavascript } from 'react-icons/si'
-import { RiGithubFill } from 'react-icons/ri'
+import im from '/im.png'
+import { FcDownload } from 'react-icons/fc'
 
 const History = () => {
    return (
       <div className={styles.history}>
          <h1>Um pouco sobre mim:</h1>
-         <p>Me chamo Pedro Henrique Rocha Gonçalves, tenho 29 anos, moro em Patos de Minas - Minas Gerais, sou apaixonado por música e tecnologia.<br></br>
-            Em 2022 estava me sentindo estagnado, sentia que podia fazer algo a mais, respirar novos ares, aprender algo novo, foi onde decidi ingressar no ramo da tecnologia.<br></br>
-            Iniciei os estudos com foco em Front End, que foi e é a área que mais me chamou a atenção e tive um maior encanto.<br></br>
-            No mesmo ano, participei de uma mentoria onde desenvolvi alguns projetos, onde também colaborei com outros colegas de curso e que também contribuíram para o meu crescimento e desenvolvimento.<br></br>
-            Sempre busco novos desafios, aprender e entendê-los, para que eu consiga melhorar 1% todos os dias! No momento estou focado nos estudos de React e TypeScript.<br></br>
-            Atualmente, estou em busca da minha primeira oportunidade de trabalho como dev.</p>
+         <p style={{ lineHeight: "28px" }}>Me chamo Pedro Henrique Rocha Gonçalves, tenho 29 anos, moro em Patos de Minas - Minas Gerais, sou apaixonado por música e tecnologia.<br></br>
+            Em 2022 estava me sentindo estagnado, sentia que podia fazer algo a mais, respirar novos ares, aprender algo novo, foi onde decidi ingressar no ramo da tecnologia,
+            logo, iniciei os estudos com foco em Front End (HTML, CSS e JavaScript).<br></br>Em abril de 2023 tomei a decisão de sair da empresa onde trabalhei por quase 7 anos para me dedicar totalmente aos estudos de programação<br></br>
+            e por fim, em agosto desse mesmo ano ingressei na faculdade no curso bacharel em Engenharia de Software.<br></br>
+            <span style={{ color: "gold" }}>No momento, estou em busca da minha primeira oportunidade de trabalhar como desenvolvedor Front End.</span></p>
+         <div style={{ marginTop: "3rem", border: "1px solid white", padding: "20px", borderRadius: "8px" }}>
+            <p>Meu Currículo Atualizado</p>
+            <a href='/Currículo Pedro H. Rocha.pdf' download="Currículo Pedro H. Rocha"><FcDownload style={{ fontSize: "40px" }} /></a>
+         </div>
+
       </div>
    )
 }
